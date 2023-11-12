b3b075891d90d1dcdb20f2d4aa503c7d7d329817
Lennart <lennart.stachowiak@code.berlin>
2023-11-12 12:12:35 +0100

    update config for different use cases

--- config.py
+++ config.py
@@ -5,10 +5,20 @@ load_dotenv()
 
 
 class Config:
-    POSTGRES_USER = environ.get('POSTGRES_USER')
-    POSTGRES_PASSWORD = environ.get('POSTGRES_PASSWORD')
-    POSTGRES_DB = environ.get('POSTGRES_DB')
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_DB}'
+    if environ.get('FLASK_ENV') == 'production':
+        SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+    elif environ.get('DATABASE_TYPE') == 'sqlite':
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+
+    elif environ.get('DATABASE_TYPE') == 'postgres':
+        POSTGRES_USER = environ.get('POSTGRES_USER')
+        POSTGRES_PASSWORD = environ.get('POSTGRES_PASSWORD')
+        POSTGRES_DB = environ.get('POSTGRES_DB')
+        SQLALCHEMY_DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_DB}'
+
+    else:
+        raise ValueError('Invalid value for FLASK_ENV')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SECRET_KEY = environ.get("SECRET_KEY")
