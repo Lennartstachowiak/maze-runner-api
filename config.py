e9ba071b7ccf69f9e0e4f5c74e919c9ead04943e
lennartstachowiak <Lennart.Stachowiak@code.berlin>
2024-04-24 16:53:39 +0200

    update config

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ load_dotenv()
 class Config:
     if environ.get("FLASK_ENV") == "production":
         SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+        DEBUG = False
 
     elif environ.get("DATABASE_TYPE") == "sqlite":
         SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
@@ -24,8 +25,3 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SECRET_KEY = environ.get("SECRET_KEY")
-
-    SESSION_TYPE = "filesystem"
-    SESSION_PERMANENT = False
-    SESSION_USE_SIGNER = True
-    # SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
